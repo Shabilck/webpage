28566438567f5c79213adf1d742ef1916d3c94d8
Shabilck <136982637+Shabilck@users.noreply.github.com>
2023-06-26 18:01:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function imh(){
+	document.getElementById('myImage').src='smile.jpg';
+}
+function ims(){
+	document.getElementById('myImage').src='sad.png';
+}
+function reply(){
+	document.write("Thank you for contacting us. We will reach out to you shortly")
+}
